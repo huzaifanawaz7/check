6eeddea7e83e3a07b3b96263912395e3475f8058
huzaifa <huzaifanawaz407@gmail.com>
2026-07-06 14:00:07 +0500

    function is added

--- new.py
+++ new.py
@@ -1,1 +1,6 @@
 print ("i have made a new file now which is untracked by the git now i am im initilizing a git in the new file ")
+# first function
+def calc_sum(a,b):
+    return a+b
+a=calc_sum(2,3)
+print(a)
