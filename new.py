ea360d589de3fe0ab31729c58ff7498d7af0e48d
huzaifa <huzaifanawaz407@gmail.com>
2026-07-06 17:26:50 +0500

    about recursion

--- new.py
+++ new.py
@@ -1,6 +1,37 @@
 print ("i have made a new file now which is untracked by the git now i am im initilizing a git in the new file ")
 # first function
-def calc_sum(a,b):
-    return a+b
-a=calc_sum(2,3)
-print(a)
+# def calc_sum(a,b):
+#     return a+b
+# a=calc_sum(2,3)
+# print(a)
+
+# recursion with examples 
+# when we c
+# all a function with  in the body of that function  of again with the base condition as a stopping thing instead of it will moving infinitely
+# one of the most basic question to understand the recursion.
+# def rec(n):  
+#     if n==6:
+#         return 
+#     print (n)
+#     rec(n+1) 
+# rec(1)
+
+# def fectorial(n):
+#     if n==1:
+#         return
+#     fectorial(n*n-1)
+# fectorial(3)
+    
+# def xyz():
+#     print("this is the recursion in the which is non sense")
+#     xyz()
+# xyz()
+n=int(input("enter the number you want to find the sum"))
+def sum(s):
+    if s==1:
+        return 1
+    else:
+        return(s+sum(s-1))
+z=sum(n)
+print(z)
+
